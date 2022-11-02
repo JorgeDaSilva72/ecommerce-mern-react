f2a845c031ed093bb35cd0c368e771d0baee60e9
JorgeDaSilva72 <73615973+JorgeDaSilva72@users.noreply.github.com>
2022-11-02 12:54:12 +0100

    Mise en place de redux toolkit

--- client/src/pages/Product.jsx
+++ client/src/pages/Product.jsx
@@ -9,6 +9,8 @@ import { mobile } from "../responsive";
 import { useLocation } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { publicRequest } from "../requestMethods";
+import { addProduct } from "../redux/cartRedux";
+import { useDispatch } from "react-redux";
 
 const Container = styled.div``;
 
@@ -126,6 +128,7 @@ const Product = () => {
   const [quantity, setQuantity] = useState(1);
   const [color, setColor] = useState("");
   const [size, setSize] = useState("");
+  const dispatch = useDispatch();
 
   useEffect(() => {
     const getProduct = async () => {
@@ -147,9 +150,7 @@ const Product = () => {
 
   const handleClick = () => {
     // UPDATE CART
-    // dispatch(
-    //   addProduct({ ...product, quantity, color, size })
-    // );
+    dispatch(addProduct({ ...product, quantity, color, size }));
   };
   return (
     <Container>
